9c5a6533bd3fed5f811766d142add7a9b4609eb5
Christina Sohn <chsohn15@gmail.com>
2020-06-15 03:07:32 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,24 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
+require 'pry'
+
 def key_for_min_value(name_hash)
 
+  if name_hash.empty?
+      return NIL
+  end
+  
+  smallest_value = 1000
+  smallest_key = " "
+      
+      
+  name_hash.each do |a, b|
+      if b < smallest_value
+        smallest_value = b
+        smallest_key = a
+
+      end
+  end
+  smallest_key
 end
