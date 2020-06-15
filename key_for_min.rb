fdb9c41faa5b95cabe5c0431ee0fce8b84ef7faa
Christina Sohn <chsohn15@gmail.com>
2020-06-15 02:58:57 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -16,9 +16,9 @@ def key_for_min_value(name_hash)
   name_hash.each do |a, b|
       if name_hash[a][b] < smallest_value
         smallest_value = name_hash[a][b]
+        binding.pry
         smallest_key = a
       end
-      binding.pry
   end
   smallest_key
 end
